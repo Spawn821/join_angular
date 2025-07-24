64158f1d519a99c0da0fa5436f947acdd401f40e
Stephan Schmidt <sekiro@p200300c33f20f875219cf5a788d4f553.dip0.t-ipconnect.de>
2025-07-24 21:35:07 +0200

    add board.component and shared-, services-, interfaces-folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //Parent Login
+  // - Login
+  // - Sign up
+  //
+  //Parent Summary
+  // - Summary
+  // - Tasks
+  // - Contatcs
+];
