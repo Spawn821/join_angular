ee102e01b4522b366e02a38be0f6b7f2fe2079dc
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-07-27 13:40:51 +0200

    initialize routing for authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 export const routes: Routes = [
-  //Parent Login
-  // - Login
-  // - Sign up
-  //
-  //Parent Summary
-  // - Summary
-  // - Tasks
-  // - Contatcs
+  {
+    path: "", component: AuthenticationComponent,
+    children: [
+      {
+        path: "login", component: LoginComponent
+      },
+      {
+        path: "signup", component: SignupComponent
+      }
+    ]
+  }
 ];
