a445b72761e0ee09b53f6d076f040710e290d09e
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-07-27 13:32:04 +0200

    structur and generate components for authentication area

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
+import { environment } from './environments/environment'
 
 import { routes } from './app.routes';
 
