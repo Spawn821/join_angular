a0f6e235d8502f5e0ac10b2fa837764c1d99c842
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-08-19 10:14:27 +0200

    forwarding signup from login and add logo, favicon

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-authentication',
-  imports: [ RouterOutlet],
+  imports: [ RouterOutlet, RouterLink],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss'
 })
 export class AuthenticationComponent {
+  private router = inject(Router);
 
+  get Router() {
+    return this.router;
+  }
 }
