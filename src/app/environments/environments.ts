3b16aef8fc68401223c82e2fedb47c2672d377cd
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-09-03 14:28:27 +0200

    test2

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,6 +3,6 @@ import { initializeApp } from "firebase/app";
 export const environment = {
     production: false,
     firebase: {
-        'hallo': 'test',
+        'hallo': 'test123',
     },
   };
