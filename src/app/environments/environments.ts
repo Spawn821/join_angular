8926152c968d0439deee062809ef317f0993f8d4
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-09-03 14:23:52 +0200

    test

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,11 +3,6 @@ import { initializeApp } from "firebase/app";
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyCnAgRGXNTHQn6Bewrl5WAWuQBsfdzG7Q0",
-        authDomain: "join-angular-68f7c.firebaseapp.com",
-        projectId: "join-angular-68f7c",
-        storageBucket: "join-angular-68f7c.firebasestorage.app",
-        messagingSenderId: "1050243719090",
-        appId: "1:1050243719090:web:6b62eb96f6526b41ef8eab"
+
     },
   };
