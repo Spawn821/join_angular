8b1c3a37263823dc96421211286a0fc7b0cf48be
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-09-29 14:41:10 +0200

    edit alignment router-wrapper at panel component

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { NavsidebarComponent } from '../shared/navsidebar/navsidebar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-panel',
   imports: [
     HeaderComponent,
     NavsidebarComponent,
-    RouterOutlet
+    RouterOutlet,
+    CommonModule
 ],
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss',
 })
-export class PanelComponent { }
+export class PanelComponent {
+
+  private router = inject(Router);
+
+  currentRoute() {
+    return this.router.url == '/panel/contacts';
+  }
+}
